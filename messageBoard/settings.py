a6e345939ed4036f93ade3df4eaadcae62f4d4e9
giftos1 <giftsydney676@gmail.com>
2024-05-29 23:16:14 +1000

    new updates for Heroku deployment

--- messageBoard/settings.py
+++ messageBoard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i5r$+4&218(jfukm3e#e#87f)12*k*k*@ew$*l-l9q1c48css4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
